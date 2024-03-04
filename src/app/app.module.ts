ea5a940fff6c3ce255299c9658d157cf723a9def
pullaposki <pullaposki@windowslive.com>
2024-03-04 11:02:47 +0200

    added for directive for displaying a list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { Conditional } from './conditional.component';
+import { PersonList } from './person-list.component';
 
 @NgModule({
-  declarations: [AppComponent, Conditional], // components
+  declarations: [AppComponent, Conditional, PersonList], // components
   imports: [BrowserModule], // modules
   providers: [], // services
   bootstrap: [AppComponent], // root component, that starts the application
---
+++ src/app/person-list.component.ts
@@ -0,0 +1,310 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'person-list',
+  templateUrl: './person-list.component.html',
+})
+export class PersonList {
+  people = [
+    {
+      name: 'Hermione Ballard',
+    },
+    {
+      name: 'Cole Mercado',
+    },
+    {
+      name: 'Stewart Powers',
+    },
+    {
+      name: 'Rebekah Good',
+    },
+    {
+      name: 'Kessie Galloway',
+    },
+    {
+      name: 'Iola Bishop',
+    },
+    {
+      name: 'Violet Fleming',
+    },
+    {
+      name: 'Susan Sullivan',
+    },
+    {
+      name: 'Pamela Meadows',
+    },
+    {
+      name: 'Buckminster Hernandez',
+    },
+    {
+      name: 'Jerome Underwood',
+    },
+    {
+      name: 'Molly Carr',
+    },
+    {
+      name: 'Melvin Hunter',
+    },
+    {
+      name: 'Dillon Hurley',
+    },
+    {
+      name: 'Anne Haney',
+    },
+    {
+      name: 'Meredith Irwin',
+    },
+    {
+      name: 'Alan Zimmerman',
+    },
+    {
+      name: 'Lilah Valencia',
+    },
+    {
+      name: 'Rae Cohen',
+    },
+    {
+      name: 'Murphy Brady',
+    },
+    {
+      name: 'Guinevere Vance',
+    },
+    {
+      name: 'Jaquelyn Battle',
+    },
+    {
+      name: 'Basia Shaffer',
+    },
+    {
+      name: 'Nigel Kirk',
+    },
+    {
+      name: 'Ashely Nguyen',
+    },
+    {
+      name: 'Carter Watkins',
+    },
+    {
+      name: 'Justina Vargas',
+    },
+    {
+      name: 'Orli Drake',
+    },
+    {
+      name: 'Alika Owen',
+    },
+    {
+      name: 'Alea Hubbard',
+    },
+    {
+      name: 'Isaac Rosales',
+    },
+    {
+      name: 'Genevieve Langley',
+    },
+    {
+      name: 'McKenzie Meyers',
+    },
+    {
+      name: 'Nathan Nash',
+    },
+    {
+      name: 'Jordan Suarez',
+    },
+    {
+      name: 'Miriam Hardy',
+    },
+    {
+      name: 'Georgia Morgan',
+    },
+    {
+      name: 'Dean Richardson',
+    },
+    {
+      name: 'Berk Erickson',
+    },
+    {
+      name: 'Gretchen Sheppard',
+    },
+    {
+      name: 'Cassady Compton',
+    },
+    {
+      name: 'Skyler Maxwell',
+    },
+    {
+      name: 'Erasmus Hunter',
+    },
+    {
+      name: 'David Hines',
+    },
+    {
+      name: 'Evan Rocha',
+    },
+    {
+      name: 'Cherokee Ashley',
+    },
+    {
+      name: 'Julian Lancaster',
+    },
+    {
+      name: 'Hayden Marks',
+    },
+    {
+      name: 'Claudia Cruz',
+    },
+    {
+      name: 'Cedric Whitaker',
+    },
+    {
+      name: 'Hayden Rich',
+    },
+    {
+      name: 'Jared Griffith',
+    },
+    {
+      name: 'Fay Bright',
+    },
+    {
+      name: 'Josiah Fisher',
+    },
+    {
+      name: 'Amal Wade',
+    },
+    {
+      name: 'Ivy Webb',
+    },
+    {
+      name: 'Craig Burt',
+    },
+    {
+      name: 'Felicia Foley',
+    },
+    {
+      name: 'Deanna Barry',
+    },
+    {
+      name: 'Nash Jefferson',
+    },
+    {
+      name: 'Xavier Mitchell',
+    },
+    {
+      name: 'Carly French',
+    },
+    {
+      name: 'Wallace Hurley',
+    },
+    {
+      name: 'Jamal Fuentes',
+    },
+    {
+      name: 'Ahmed Wilkerson',
+    },
+    {
+      name: 'Carla Myers',
+    },
+    {
+      name: 'Peter Allen',
+    },
+    {
+      name: 'Leandra Watkins',
+    },
+    {
+      name: 'Jayme Taylor',
+    },
+    {
+      name: 'Randall Pena',
+    },
+    {
+      name: 'Alexa Stafford',
+    },
+    {
+      name: 'Lila Gamble',
+    },
+    {
+      name: 'May Cobb',
+    },
+    {
+      name: 'Hamish Tyler',
+    },
+    {
+      name: 'Ocean Welch',
+    },
+    {
+      name: 'Rhonda Lambert',
+    },
+    {
+      name: 'Lev Lawrence',
+    },
+    {
+      name: 'Cassady Bauer',
+    },
+    {
+      name: 'Maite Boyd',
+    },
+    {
+      name: 'Leonard Mcmahon',
+    },
+    {
+      name: 'Bevis Key',
+    },
+    {
+      name: 'Thaddeus Byers',
+    },
+    {
+      name: 'Carly Osborn',
+    },
+    {
+      name: 'Marshall Maynard',
+    },
+    {
+      name: 'Shad Lynn',
+    },
+    {
+      name: 'Charles Welch',
+    },
+    {
+      name: 'Beau Osborn',
+    },
+    {
+      name: 'Clinton Quinn',
+    },
+    {
+      name: 'Carson Ferrell',
+    },
+    {
+      name: 'Nicholas Nicholson',
+    },
+    {
+      name: 'Jackson Phillips',
+    },
+    {
+      name: 'Alexa Kemp',
+    },
+    {
+      name: 'Ramona Porter',
+    },
+    {
+      name: 'Kameko Powers',
+    },
+    {
+      name: 'Aurora Vinson',
+    },
+    {
+      name: 'Thor Dunlap',
+    },
+    {
+      name: 'Regan Barber',
+    },
+    {
+      name: 'Chloe Owen',
+    },
+    {
+      name: 'Miranda Gonzales',
+    },
+    {
+      name: 'Chadwick Mayer',
+    },
+  ];
+}
